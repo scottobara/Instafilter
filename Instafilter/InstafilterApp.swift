b2c7a3d7b5be63cd4d1017a5fa645439620b5853
Scott Obara <scott@rubbscreations.com>
2021-02-15 20:32:21 +1100

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Scott Obara on 15/2/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
