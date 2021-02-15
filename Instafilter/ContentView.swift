4e15cac5112a43a3e44c6fc1dbc3758ab2a23d12
Scott Obara <scott@rubbscreations.com>
2021-02-16 07:00:24 +1100

    Integrating Core Image with SwiftUI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,43 +5,58 @@
 //  Created by Scott Obara on 15/2/21.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount: CGFloat = 0
-    @State private var showingActionSheet = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
 
     var body: some View {
-        let blur = Binding<CGFloat>(
-            get: {
-                self.blurAmount
-            },
-            set: {
-                self.blurAmount = $0
-                print("New value is \(self.blurAmount)")
-            }
-        )
-
-        return VStack {
-            Text("Action sheet!")
-                .frame(width: 300, height: 300)
-                .background(backgroundColor)
-                .onTapGesture {
-                    self.showingActionSheet = true
-                }
-                .actionSheet(isPresented: $showingActionSheet) {
-                    ActionSheet(title: Text("Change background"), message: Text("Select a new color"), buttons: [
-                        .default(Text("Red")) { self.backgroundColor = .red },
-                        .default(Text("Green")) { self.backgroundColor = .green },
-                        .default(Text("Blue")) { self.backgroundColor = .blue },
-                        .cancel()
-                    ])
-                }
-                .blur(radius: blurAmount)
-
-            Slider(value: blur, in: 0...20)
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
         }
+        .onAppear(perform: loadImage)
+    }
+
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+
+        let context = CIContext()
+
+//        let currentFilter = CIFilter.sepiaTone()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.intensity = 1
+        
+//        let currentFilter = CIFilter.pixellate()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.scale = 50
+
+        let currentFilter = CIFilter.crystallize()
+        currentFilter.inputImage = beginImage
+        currentFilter.radius = 100
+        
+//        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
+//        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+//        currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
+//        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
+        
+        // get a CIImage from our filter or exit if that fails
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        // attempt to get a CGImage from our CIImage
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            // convert that to a UIImage
+            let uiImage = UIImage(cgImage: cgimg)
+
+            // and convert that to a SwiftUI image
+            image = Image(uiImage: uiImage)
+        }
+        
+        
     }
 }
 
